feb0708c9bfa1658eb8c849ac57381635cf2f9b0
blakehartin <blakehartin@bh>
2025-07-04 18:19:28 -0700

    fix index.d.ts generic errors

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
-export function cryptoRandom(size: any): Uint8Array<any>;
-export function cryptoNewSeed(): Uint8Array<any>;
-export function cryptoExpandSeed(seedArray: any): Uint8Array<ArrayBuffer>;
+export function cryptoRandom(size: any): Uint8Array;
+export function cryptoNewSeed(): Uint8Array;
+export function cryptoExpandSeed(seedArray: any): Uint8Array;
 export function cryptoNewKeyPair(): KeyPair;
 export function cryptoNewKeyPairFromSeed(expandedSeedArray: any): KeyPair;
-export function cryptoSign(messageArray: any, privateKeyArray: any): Uint8Array<ArrayBuffer>;
+export function cryptoSign(messageArray: any, privateKeyArray: any): Uint8Array;
 export function cryptoVerify(messageArray: any, sigArray: any, publicKeyArray: any): boolean;
 declare class KeyPair {
     constructor(privateKey: any, publicKey: any);
